faa71d29cf1aeadbc457f09ca8820e8a707d65ca
Roberto <Roberto@192.168.0.12>
2018-05-01 21:07:55 -0600

    Se han cambiado un vergo de mierdas :v

--- src/primerparcialpoo/Validaciones.java
+++ src/primerparcialpoo/Validaciones.java
@@ -7,7 +7,7 @@ package primerparcialpoo;
 
 /**
  *
- * @author Roberto
+ * @author Roberto Alexander Rubio Castillo <00259517@uca.edu.sv>
  */
 /*Validar
     -Un mismo cliente no puede reservar mas de 2 habitaciones
@@ -16,22 +16,50 @@ package primerparcialpoo;
     -No puede existir campos vacios en una reservacion
     -try catch por si los valores ingresados son incorrectos ejemplo un string en un int
 */
+//sintaxis para las fechas dd/mm/aaaa
 public class Validaciones {
-    char[] Pisos = {'A','B','C','D','E','F'};
-    public boolean MaximoTiempoReservacion(int CantDias){
+
+    public boolean MaximoTiempoReservacion(int CantDias){//Validacion de tiempo de reservacion
         if (CantDias > 7){
             System.out.println("No puede registrarse por mas de 7 dias");
             return false;
         }
         return true;
     }
-    public boolean HabitacionReservada(char[] Habitacion){
+    public boolean HabitacionReservada(char[] Habitacion){//Validacion que la habitacion exista
+        char[] Pisos = {'A','B','C','D','E','F'};
         for (int i=0; i<7; i++){
             if (Habitacion[0] != Pisos[i]){
-                System.out.println("No puede reservar una habitacion que no existe");
+                System.out.println("No puede reservar una habitacion en un piso que no existe");
                 return false;
             }
         }
+        if (Habitacion[1] <= 0 || Habitacion[1] > 10){
+            System.out.println("No puede ingresar ese numero de habitacion");
+            return false;
+        }
+        return true;
+    }
+    public boolean ValidarCantidadDeReservaciones(String Nombre, String FechaIngresada){// validador de cantidad de reservaciones, aun hay que cambiar parametros para que reciva la lista de reservaciones
+        char[] FechaIngresadaC = FechaIngresada.toCharArray();//Fecha Ingresada se convierte en arreglo
+        int DiaI, DiaE, MesI, MesE, AnioI, AnioE; //DiaI=DiaIngresado, DiaE=DiaExistente
+        String aux, NombreE;//NombreE=Nombre que tenemos que tener en reservaciones
+        aux = new StringBuilder().append(FechaIngresadaC[0]).append(FechaIngresadaC[1]).toString();
+        DiaI = Integer.parseInt(aux);
+        aux = new StringBuilder().append(FechaIngresadaC[3]).append(FechaIngresadaC[4]).toString();
+        MesI = Integer.parseInt(aux);
+        aux = new StringBuilder().append(FechaIngresadaC[6]).append(FechaIngresadaC[7]).append(FechaIngresadaC[8]).append(FechaIngresadaC[9]).toString();
+        AnioI = Integer.parseInt(aux);
+/*      int cont = 0;
+        While (Reservaciones){
+            String nombre Reservaciones.getnombre;
+            if(Nombre == nombre){
+                cont++;   
+            if (Nombre == NombreE && DiaI == DiaE && MesI == MesE && AnioI == AnioE && cont >= 2){
+                    System.out.println("Usted no puede tener mas de dos reservaciones continuas");
+                    return false;
+                }
+*/
         return true;
     }
     public boolean ValidarCorreo(String Correo){
