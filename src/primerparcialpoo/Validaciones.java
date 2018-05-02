8602b3280e971f914a8bb0b7f9677dfbd6d79cfe
Roberto <Roberto@192.168.0.12>
2018-05-01 18:26:19 -0600

    Se han agregado varias validaciones

--- src/primerparcialpoo/Validaciones.java
+++ src/primerparcialpoo/Validaciones.java
@@ -10,7 +10,6 @@ package primerparcialpoo;
  * @author Roberto
  */
 /*Validar
-    -Numero maximo de una reservacion es solo 7 dias
     -Un mismo cliente no puede reservar mas de 2 habitaciones
     -El ingreso de datos de una habitacion no tiene que sobrepasar los limites del hotel, tampoco ser valores de habitaciones 0 o negativos
     -La seleccion del paquete queda en la reservacion pero un cliente no puede escoger mas de un paquete
@@ -18,5 +17,40 @@ package primerparcialpoo;
     -try catch por si los valores ingresados son incorrectos ejemplo un string en un int
 */
 public class Validaciones {
-    
+    char[] Pisos = {'A','B','C','D','E','F'};
+    public boolean MaximoTiempoReservacion(int CantDias){
+        if (CantDias > 7){
+            System.out.println("No puede registrarse por mas de 7 dias");
+            return false;
+        }
+        return true;
+    }
+    public boolean HabitacionReservada(char[] Habitacion){
+        for (int i=0; i<7; i++){
+            if (Habitacion[0] != Pisos[i]){
+                System.out.println("No puede reservar una habitacion que no existe");
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean ValidarCorreo(String Correo){
+        char[] CorreoC = Correo.toCharArray();
+        for (int i=0; i<CorreoC.length; i++){
+            if (CorreoC[i] == '@'){
+                return true;
+            }
+        }
+        System.out.println("Su correo no es valido");
+        return false;
+    }
+    public boolean ValidarTelefono(String Telefono){
+        char[] TelefonoC = Telefono.toCharArray();
+
+        if (8 > TelefonoC.length){
+            return false;
+        }
+        return true;
+    }
+
 }
