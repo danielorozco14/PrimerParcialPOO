be143e765321aae6efb860ab58d20bbab55e6d8c
Karicha Lobato <00002517@uca.edu.sv>
2018-05-03 17:06:51 -0600

    descubri que UUDI hace lo mismo que hashCode, solo con UDDI basta para que no se repita ningun código

--- src/primerparcialpoo/Factura.java
+++ src/primerparcialpoo/Factura.java
@@ -49,40 +49,8 @@ public class Factura {
     //SACANDO TOTAL 
     public void TotalPagar() {
         //SUMANDO LOS SERVICIOS PARA SACAR UN TOTAL A PAGAR POR CUENTA DEL HUESPED
-        this.TotalAPagar += this.servicios.getPrecioHabitacion()+this.servicios.getVariacionTemporada();
+        this.TotalAPagar += this.servicios.getPrecioHabitacion()+ this.servicios.getVariacionTemporada();
 
     }
     
-    /*
-    Este método viene a complementar al método equals 
-    y sirve para comparar objetos de una forma más rápida 
-    en estructuras Hash ya que únicamente nos devuelve un número entero. 
-    Cuando Java compara dos objetos en estructuras de tipo hash (HashMap, HashSet etc) 
-    primero invoca al método hashcode y luego el equals.
-    */
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 23 * hash + Objects.hashCode(this.idFactura);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Factura other = (Factura) obj;
-        if (!Objects.equals(this.idFactura, other.idFactura)) {
-            return false;
-        }
-        return true;
-    }
-
 }
